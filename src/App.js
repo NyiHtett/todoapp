cffe31d03c0914f0c58b67e08802b8b553acdf96
Nyi Htet <nyihtet4dev@icloud.com>
2023-07-20 14:45:40 -0700

    Day 22

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-
+import {react, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todolist, setTodolist ] = useState([]);
+  const [keytrack, setKeytrack ] = useState('');
+
+  const handleClicker = () => {
+    if(keytrack.trim() !== ''){
+      setTodolist([...todolist, keytrack]);
+      setKeytrack('');
+    }
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1> ToDoList </h1>
       </header>
+
+      <div>
+        <input 
+        type = "text"
+        value = {keytrack}
+        onChange={(e)=>{
+          setKeytrack(e.target.value);
+        }}
+        placeholder="Enter a task"
+        /> 
+        <button onClick={handleClicker}> Submit</button>
+        
+      </div>
+      <div>
+        {todolist}
+      </div>
+      
     </div>
   );
 }
